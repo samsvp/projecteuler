f342c8385bbd9e1160034d8d885a8122cc330f29
Samuel Simplicio <samuelsimplicio@poli.ufrj.br>
2021-12-30 22:41:08 -0300

    More solutions

--- 10.hs
+++ 10.hs
@@ -1,8 +1,6 @@
 {-
-By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, 
-we can see that the 6th prime is 13.
-
-What is the 10 001st prime number?
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+Find the sum of all the primes below two million.
 -}
 
 
@@ -18,7 +16,8 @@ isPrimeLoop n 1 = True
 isPrimeLoop n m | mod n m /= 0 = isPrimeLoop n (m - 1)
 isPrimeLoop n m = False
 
-primes = [n | n <- 2:[3,5..], isPrime n]
+primes = 2 : [n | n <- [3,5..], isPrime n]
+s =  sum (takeWhile (< 2000000) primes)
 
 main :: IO ()
-main = print (primes !! 10000)
+main = print s
---
+++ 9.hs
@@ -0,0 +1,21 @@
+{-
+A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+a^2 + b^2 = c^2
+
+For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+
+There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+Find the product abc.
+-}
+
+
+c :: Floating a => a -> a -> a
+c a b = sqrt(a^2 + b^2)
+
+triplet = head [ a * b * c a b
+    | a <- [1..999], b <- [a..999], a + b + c a b == 1000]
+
+
+
+main :: IO ()
+main = print triplet
